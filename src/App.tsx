4f598ba898f92b716d159fe825025220bc66907e
yujia <yujia4513@qq.com>
2024-12-23 11:23:06 +0800

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import MapPage from './pages/MapPage.tsx';
+import { HashRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <MapPage />
-    </div>
+    </Router>
   );
 };
 
